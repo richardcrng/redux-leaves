20ee34b5736e2f2920041c329cfe0b3cd7f34af5
Richard Ng <richardcrng@gmail.com>
2019-04-04 19:27:24 +0100

    install redux devtolls

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <Provider store={store}>
