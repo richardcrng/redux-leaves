d40f790d98ef2336a4cb3cc8124c989ad6b1ac9c
Richard Ng <richardcrng@gmail.com>
2019-04-05 09:12:49 +0100

    try setting up leaf reducer to clear and reset from ancestor

--- src/redux-leaves/reducer/leaf/index.js
+++ src/redux-leaves/reducer/leaf/index.js
@@ -15,11 +15,7 @@ export const makeReducerLeaf = (prefix, initialState) => {
   }
 }
 
-const reducerLeaf = ({
-  prefix = "app",
-  route,
-  initialState
-}) => {
+const reducerLeaf = ({ prefix = "app", route, initialState }) => {
   const reducer = vanillaReducerLeaf({ prefix, route, initialState })
   return withActions(reducer, { prefix, route })
 }
@@ -39,55 +35,61 @@ const withActions = (reducer, { prefix, route }) => {
   return reducer
 }
 
-const vanillaReducerLeaf = ({
-  prefix = "app",
-  route,
-  initialState
-}) => {
+const vanillaReducerLeaf = ({ prefix = "app", route, initialState }) => {
   return (
     state = initialState,
     { type, payload = null, meta = null } = {}
   ) => {
 
-    if (!_.startsWith(type, prefix)) return state
-    if (route && !_.startsWith(type, pathJoin([prefix, route]))) return state
+    const dropPrefixFromType = _.replace(type, prefix, '')
+    const { route: actionRoute, modifier } = findActionRouteAndModifier(dropPrefixFromType, meta);
 
-    const suffix = _.replace(type, prefix, '')
-    const { path, modifier } = findPathAndModifier(suffix, meta);
+    const actionPath = pathJoin([prefix, actionRoute])
+    const pathToLeaf = pathJoin([prefix, route])
 
-    switch (modifier) {
-      case atomicActions.CLEAR:
-        return null;
-      case atomicActions.INCREMENT:
-        const increment = (typeof payload === "number") ? payload : 1
-        return state + increment
-      case atomicActions.OFF:
-        return false
-      case atomicActions.ON:
-        return true
-      case atomicActions.RESET:
-        return initialState
-      case atomicActions.SET:
-        return payload
-      case atomicActions.TOGGLE:
-        return !state
-      default:
-        return state
+    // CLEAR and RESET if pathToLeaf starts with actionPath
+    //    (so that parent branch can clear)
+    if (_.startsWith(pathToLeaf, actionPath)) {
+      switch (modifier) {
+        case atomicActions.CLEAR: return null
+        case atomicActions.RESET: return initialState
+      }
     }
+
+    // Only perform other actions when pathToLeaf is identical
+    //    to actionPath
+    if (pathToLeaf == actionPath) {
+      switch (modifier) {
+        case atomicActions.INCREMENT:
+          const increment = (typeof payload === "number") ? payload : 1
+          return state + increment
+        case atomicActions.OFF:
+          return false
+        case atomicActions.ON:
+          return true
+        case atomicActions.SET:
+          return payload
+        case atomicActions.TOGGLE:
+          return !state
+        default:
+          return state
+      }
+    }
+
+    return state
   }
 }
 
-const findPathAndModifier = (trimmedType, metaPath) => {
-  // Takes in something like "/list/CLEAR"
-  const arr = trimmedType.split('/')
-  // Remove empty strings
+const findActionRouteAndModifier = (pathWithActionString = "example/CLEAR", metaPath) => {
+  const arr = pathWithActionString.split('/')
+  // Remove empty strings, e.g. if we've split "/example/CLEAR/"
   const compacted = _.compact(arr)
   // Pop off the last element - this is the modifier
   const modifier = compacted.pop()
   // Push meta path modifier which action might have
   if (metaPath) compacted.push(metaPath)
   return {
-    path: compacted.join('.'),
+    route: compacted,
     modifier
   }
 }
