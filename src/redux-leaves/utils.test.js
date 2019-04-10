76372f75431384b220201231806af2631e50f69d
Richard Ng <richardcrng@gmail.com>
2019-04-10 11:41:42 +0100

    write bdd tests for pathJoin utility

--- src/redux-leaves/utils.test.js
+++ src/redux-leaves/utils.test.js
@@ -1,22 +1,85 @@
 import { pathJoin } from "./utils";
 
-describe("pathJoin utility", () => {
-  describe("called with one array", () => {
-    it("produces an appropriate path when provided an array of simple strings", () => {
-      const result = pathJoin(["app", "path", "example"])
-      expect(result).toBe("app/path/example")
-    })
+describe("pathJoin", () => {
 
-    it("produces an appropriate path when provided an array of arrays of simple strings", () => {
-      const result = pathJoin(["app", ["some", "nested", "path"], "example"])
-      expect(result).toBe("app/some/nested/path/example")
-    })
+  describe("**Feature**: when called with one flat array, returns a string joining them with /'s", () => {
 
-    it("cleans up when elements have trailing join characters", () => {
-      const result = pathJoin(["/app/", ["some/", "nested/other", "/path"], "example"])
-      expect(result).toBe("app/some/nested/other/path/example")
+    describe("GIVEN an array of ['app', 'path', 'example']", () => {
+      const path = ["app", "path", "example"]
+
+      describe("WHEN pathJoin is executed", () => {
+        const result = pathJoin(path)
+
+        test("THEN it returns 'app/path/example'", () => {
+          expect(result).toBe("app/path/example")
+        })
+      })
     })
   })
 
+  describe("**Feature**: when called with one nested array, returns a string joining the ultimate elemnts with /'s", () => {
 
+    describe("GIVEN an array of ['app', ['path', 'example']]", () => {
+      const path = ["app", ["path", "example"]]
+
+      describe("WHEN pathJoin is executed", () => {
+        const result = pathJoin(path)
+
+        test("THEN it returns 'app/path/example'", () => {
+          expect(result).toBe("app/path/example")
+        })
+      })
+    })
+
+    describe("GIVEN an array of [['app'], 'path', 'example']", () => {
+      const path = [["app"], "path", "example"]
+
+      describe("WHEN pathJoin is executed", () => {
+        const result = pathJoin(path)
+
+        test("THEN it returns 'app/path/example'", () => {
+          expect(result).toBe("app/path/example")
+        })
+      })
+    })
+
+    describe("GIVEN an array of [[['app'], 'nested'], 'path', 'example']", () => {
+      const path = [[["app"], 'nested'], "path", "example"]
+
+      describe("WHEN pathJoin is executed", () => {
+        const result = pathJoin(path)
+
+        test("THEN it returns 'app/nested/path/example'", () => {
+          expect(result).toBe("app/nested/path/example")
+        })
+      })
+    })
+  })
+
+  describe("**Feature**: it cleans up trailing separators in the array provided", () => {
+
+    describe("GIVEN an array of ['app/', '/path', '/example/']", () => {
+      const path = ["app/", "/path", "/example/"]
+
+      describe("WHEN pathJoin is executed", () => {
+        const result = pathJoin(path)
+
+        test("THEN it returns 'app/path/example'", () => {
+          expect(result).toBe("app/path/example")
+        })
+      })
+    })
+
+    describe("GIVEN an array of ['app/', '/path', '/nested/example/']", () => {
+      const path = ["app/", "/path", "/nested/example/"]
+
+      describe("WHEN pathJoin is executed", () => {
+        const result = pathJoin(path)
+
+        test("THEN it returns 'app/path/nested/example'", () => {
+          expect(result).toBe("app/path/nested/example")
+        })
+      })
+    })
+  })
 })
