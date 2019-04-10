75a0a4c826d0e92e882356f866418a0f530b7850
Richard Ng <richardcrng@gmail.com>
2019-04-10 11:33:50 +0100

    wip fixing tests

--- src/redux-leaves/reducer/leaf/vanilla/__tests__/vanillaReducerLeaf.RESET.test.js
+++ src/redux-leaves/reducer/leaf/vanilla/__tests__/vanillaReducerLeaf.RESET.test.js
@@ -44,22 +44,22 @@ describe("**Feature**: created reducer does not returs null when an action is di
   describe("GIVEN a reducer from { prefix: 'app/prefix', route: ['path', 'deep'], initialState: true }", () => {
     const reducer = vanillaReducerLeaf({ prefix: "app/prefix", route: ["path, deep"], initialState: true })
 
-    // describe("WHEN the reducer is called with state = true and action type 'app/prefix/route/path/wrong/RESET'", () => {
-    //   const result = reducer(true, { type: "app/prefix/route/path/wrong/RESET" })
+    describe("WHEN the reducer is called with state = true and action type 'app/prefix/route/path/wrong/RESET'", () => {
+      const result = reducer(true, { type: "app/prefix/route/path/wrong/RESET" })
 
-    //   test("THEN it returns true", () => {
-    //     expect(result).toBe(true)
-    //   })
-    // })
+      test("THEN it returns true", () => {
+        expect(result).toBe(true)
+      })
+    })
 
 
-    // describe("WHEN the reducer is called with state = 'string' and action type 'will/not/RESET'", () => {
-    //   const string = 'string'
-    //   const result = reducer(string, { type: "will/not/RESET" })
+    describe("WHEN the reducer is called with state = 'string' and action type 'will/not/RESET'", () => {
+      const string = 'string'
+      const result = reducer(string, { type: "will/not/RESET" })
 
-    //   test("THEN it returns the same string", () => {
-    //     expect(result).toBe(string)
-    //   })
-    // })
+      test("THEN it returns the same string", () => {
+        expect(result).toBe(string)
+      })
+    })
   })
 })
