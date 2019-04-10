7b83a223b8ea802ba13b2cade58f13b832050fe2
Richard Ng <richardcrng@gmail.com>
2019-04-10 15:29:10 +0100

    slight refactor of vanilalReducerLeaf.test.js

--- src/redux-leaves/reducer/leaf/makeReducerLeaf.test.js
+++ src/redux-leaves/reducer/leaf/makeReducerLeaf.test.js
@@ -8,3 +8,27 @@ describe("makeReducerLeaf", () => {
     expect(typeof result).toBe("function")
   })
 })
+
+// describe("**Feature** A vanilla reducer leaf can access action creator functions needed to update its own store", () => {
+
+//   describe("GIVEN reducer = vanillaReducerLeaf({ prefix: 'app/prefix', route: ['some', 'route'], initialState: 0 })", () => {
+//     const reducer = vanillaReducerLeaf({ prefix: "app/prefix", route: ["some", "route"], initialState: 0 })
+
+//     describe("AND a store that is created from the reducer with no preloaded state", () => {
+//       let store
+//       beforeEach(() => {
+//         store = createStore(reducer)
+//       })
+
+//       describe("WHEN the store is dispatched reducer.increment()", () => {
+//         beforeEach(() => {
+//           store.dispatch(reducer.increment())
+//         })
+
+//         test("THEN the store has state of 1", () => {
+//           expect(store.getState()).toBe(1)
+//         })
+//       })
+//     })
+//   })
+// })
