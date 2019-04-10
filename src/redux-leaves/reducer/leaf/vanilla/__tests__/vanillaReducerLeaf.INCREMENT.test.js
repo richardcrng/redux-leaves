ac7f9372455dd5e856027bf276f06bdb9697e7fd
Richard Ng <richardcrng@gmail.com>
2019-04-10 14:06:04 +0100

    add some tests including meta property on actions

--- src/redux-leaves/reducer/leaf/vanilla/__tests__/vanillaReducerLeaf.INCREMENT.test.js
+++ src/redux-leaves/reducer/leaf/vanilla/__tests__/vanillaReducerLeaf.INCREMENT.test.js
@@ -1,6 +1,6 @@
 import { vanillaReducerLeaf } from "..";
 
-describe("**Feature**: created reducer increments state when an action is dispatched with '.../INCREMENT' at the exact path implied by the prefix and route", () => {
+describe("**Feature**: created reducer increments state when an action is dispatched with '.../INCREMENT' at the exact path implied by the prefix, route and meta", () => {
 
   describe("GIVEN a reducer from { prefix: 'app/prefix', route: ['path', 'deep'], initialState: 10 }", () => {
     const reducer = vanillaReducerLeaf({ prefix: "app/prefix", route: ["path", "deep"], initialState: 10 })
@@ -10,6 +10,29 @@ describe("**Feature**: created reducer increments state when an action is dispat
 
       test("THEN it returns 6", () => {
         expect(result).toBe(6)
+      })
+    })
+
+    describe("WHEN the reducer is called with state = 5, action type 'app/prefix/path/INCREMENT' and meta = 'deep'", () => {
+      const result = reducer(5, { type: "app/prefix/path/INCREMENT", meta: "deep" })
+
+      test("THEN it returns 6", () => {
+        expect(result).toBe(6)
+      })
+    })
+  })
+})
+
+describe("**Feature**: created reducer ignores actions of type '.../INCREMENT' with irrelevant path implied by the prefix, route and meta", () => {
+
+  describe("GIVEN a reducer from { prefix: 'app/prefix', route: ['path', 'deep'], initialState: 10 }", () => {
+    const reducer = vanillaReducerLeaf({ prefix: "app/prefix", route: ["path", "deep"], initialState: 10 })
+
+    describe("WHEN the reducer is called with state = 5, action type 'app/prefix/path/deep/INCREMENT' and meta = 'junk'", () => {
+      const result = reducer(5, { type: "app/prefix/path/deep/INCREMENT", meta: "junk" })
+
+      test("THEN it returns 5", () => {
+        expect(result).toBe(5)
       })
     })
   })
