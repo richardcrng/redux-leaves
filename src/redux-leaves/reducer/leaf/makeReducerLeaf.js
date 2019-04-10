f720c460ca621fe51d0773d1169a39898454df20
Richard Ng <richardcrng@gmail.com>
2019-04-10 15:38:43 +0100

    extract out reducerLeaf into separate file

--- src/redux-leaves/reducer/leaf/makeReducerLeaf.js
+++ src/redux-leaves/reducer/leaf/makeReducerLeaf.js
@@ -1,8 +1,5 @@
 import _ from 'lodash';
-import { pathJoin } from '../../utils';
-import { atomicActions } from '../../actions/atomic/';
-import { makeActionCreator } from '../../actions/creator/';
-import { vanillaReducerLeaf } from './vanilla/';
+import { reducerLeaf } from './reducerLeaf';
 
 
 export const makeReducerLeaf = (initialState, prefix = "") => {
@@ -15,26 +12,4 @@ export const makeReducerLeaf = (initialState, prefix = "") => {
         : _.get(initialState, route.join('.'), null)
     })
   }
-}
-
-const reducerLeaf = ({ prefix = "app", route, initialState }) => {
-  const reducer = vanillaReducerLeaf({ prefix, route, initialState })
-  return withActions(reducer, { prefix, route })
-}
-
-const withActions = (reducer, { prefix, route }) => {
-  const leafAction = type => makeActionCreator(pathJoin([prefix, route, type]))
-
-  reducer.apply = leafAction(atomicActions.APPLY)
-  reducer.clear = leafAction(atomicActions.CLEAR)
-  reducer.increment = leafAction(atomicActions.INCREMENT)
-  reducer.off = leafAction(atomicActions.OFF)
-  reducer.on = leafAction(atomicActions.ON)
-  reducer.push = leafAction(atomicActions.PUSH)
-  reducer.reset = leafAction(atomicActions.RESET)
-  reducer.set = leafAction(atomicActions.SET)
-  reducer.update = leafAction(atomicActions.SET)
-  reducer.toggle = leafAction(atomicActions.TOGGLE)
-
-  return reducer
 }
