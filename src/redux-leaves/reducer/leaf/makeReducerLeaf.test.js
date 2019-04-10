971e3a1ab3bcd2d3cb5478459fcb6155a42446dd
Richard Ng <richardcrng@gmail.com>
2019-04-10 16:10:13 +0100

    check action.type constants in makeReducerLeaf tests

--- src/redux-leaves/reducer/leaf/makeReducerLeaf.test.js
+++ src/redux-leaves/reducer/leaf/makeReducerLeaf.test.js
@@ -1,7 +1,7 @@
 import { makeReducerLeaf } from './';
 import { createStore } from 'redux';
 
-describe("**Feature**: returns a reducer leaf function that looks at the relevant property in supplied initialState shape, optional prefix and optional invoked route", () => {
+describe("**Feature**: returns a reducer leaf function that looks at the relevant property in supplied initialState shape, prefix and invoked route", () => {
   describe("GIVEN initialState = { bool: true, counter: 10, foo: 'bar' }", () => {
     const initialState = { bool: true, counter: 10, foo: 'bar' }
 
@@ -22,9 +22,13 @@ describe("**Feature**: returns a reducer leaf function that looks at the relevan
             expect(typeof reducer).toBe("function")
           })
 
-          test("THEN reducer has an 'off' property that is a function", () => {
+          test("AND reducer has an 'off' property that is a function", () => {
             expect(reducer.off).toBeDefined()
             expect(typeof reducer.off).toBe("function")
+          })
+
+          test("AND reducer.off.type is 'app/prefix/bool/OFF'", () => {
+            expect(reducer.off.type).toBe("app/prefix/bool/OFF")
           })
 
           describe("AND store = createStore(reducer)", () => {
@@ -52,9 +56,13 @@ describe("**Feature**: returns a reducer leaf function that looks at the relevan
             expect(typeof reducer).toBe("function")
           })
 
-          test("THEN reducer has an 'increment' property that is a function", () => {
+          test("AND reducer has an 'increment' property that is a function", () => {
             expect(reducer.increment).toBeDefined()
             expect(typeof reducer.increment).toBe("function")
+          })
+
+          test("AND reducer.increment.type is 'app/prefix/counter/INCREMENT'", () => {
+            expect(reducer.increment.type).toBe("app/prefix/counter/INCREMENT")
           })
 
           describe("AND store = createStore(reducer)", () => {
@@ -82,9 +90,13 @@ describe("**Feature**: returns a reducer leaf function that looks at the relevan
             expect(typeof reducer).toBe("function")
           })
 
-          test("THEN reducer has an 'update' property that is a function", () => {
+          test("AND reducer has an 'update' property that is a function", () => {
             expect(reducer.update).toBeDefined()
             expect(typeof reducer.update).toBe("function")
+          })
+
+          test("AND reducer.update.type is 'app/prefix/foo/SET'", () => {
+            expect(reducer.update.type).toBe("app/prefix/foo/SET")
           })
 
           describe("AND store = createStore(reducer)", () => {
@@ -101,6 +113,122 @@ describe("**Feature**: returns a reducer leaf function that looks at the relevan
               test("THEN the store has a state of 'foobar'", () => {
                 expect(store.getState()).toBe('foobar')
               })
+            })
+          })
+        })
+      })
+    })
+  })
+})
+
+describe("**Feature**: returns a reducer leaf function that looks at the relevant property in supplied initialState shape and invoked route without needing a prefix", () => {
+  describe("GIVEN initialState = { bool: true, counter: 10, foo: 'bar' }", () => {
+    const initialState = { bool: true, counter: 10, foo: 'bar' }
+
+    describe("WHEN reducerLeaf = makeReducerLeaf(initialState)", () => {
+      const reducerLeaf = makeReducerLeaf(initialState)
+
+      test("THEN reducerLeaf is a function", () => {
+        expect(typeof reducerLeaf).toBe("function")
+      })
+
+      describe("AND reducer = reducerLeaf('bool')", () => {
+        const reducer = reducerLeaf('bool')
+
+        test("THEN reducer is a function", () => {
+          expect(typeof reducer).toBe("function")
+        })
+
+        test("AND reducer has an 'off' property that is a function", () => {
+          expect(reducer.off).toBeDefined()
+          expect(typeof reducer.off).toBe("function")
+        })
+
+        test("AND reducer.off.type is 'bool/OFF'", () => {
+          expect(reducer.off.type).toBe("bool/OFF")
+        })
+
+        describe("AND store = createStore(reducer)", () => {
+          let store
+          beforeEach(() => store = createStore(reducer))
+
+          test("THEN the store has a state of true", () => {
+            expect(store.getState()).toBe(true)
+          })
+
+          describe("AND store is dispatched reducer.off()", () => {
+            beforeEach(() => store.dispatch(reducer.off()))
+
+            test("THEN the store has a state of false", () => {
+              expect(store.getState()).toBe(false)
+            })
+          })
+        })
+      })
+
+      describe("AND reducer = reducerLeaf('counter')", () => {
+        const reducer = reducerLeaf('counter')
+
+        test("THEN reducer is a function", () => {
+          expect(typeof reducer).toBe("function")
+        })
+
+        test("AND reducer has an 'increment' property that is a function", () => {
+          expect(reducer.increment).toBeDefined()
+          expect(typeof reducer.increment).toBe("function")
+        })
+
+        test("AND reducer.increment.type is 'counter/INCREMENT'", () => {
+          expect(reducer.increment.type).toBe("counter/INCREMENT")
+        })
+
+        describe("AND store = createStore(reducer)", () => {
+          let store
+          beforeEach(() => store = createStore(reducer))
+
+          test("THEN the store has a state of 10", () => {
+            expect(store.getState()).toBe(10)
+          })
+
+          describe("AND store is dispatched reducer.increment(5)", () => {
+            beforeEach(() => store.dispatch(reducer.increment(5)))
+
+            test("THEN the store has a state of 15", () => {
+              expect(store.getState()).toBe(15)
+            })
+          })
+        })
+      })
+
+      describe("AND reducer = reducerLeaf('foo')", () => {
+        const reducer = reducerLeaf('foo')
+
+        test("THEN reducer is a function", () => {
+          expect(typeof reducer).toBe("function")
+        })
+
+        test("AND reducer has an 'update' property that is a function", () => {
+          expect(reducer.update).toBeDefined()
+          expect(typeof reducer.update).toBe("function")
+        })
+
+        test("AND reducer.update.type is 'foo/SET'", () => {
+          expect(reducer.update.type).toBe("foo/SET")
+        })
+
+        describe("AND store = createStore(reducer)", () => {
+          let store
+          beforeEach(() => store = createStore(reducer))
+
+          test("THEN the store has a state of 'bar'", () => {
+            expect(store.getState()).toBe('bar')
+          })
+
+          describe("AND store is dispatched reducer.update('foobar')", () => {
+            beforeEach(() => store.dispatch(reducer.update('foobar')))
+
+            test("THEN the store has a state of 'foobar'", () => {
+              expect(store.getState()).toBe('foobar')
             })
           })
         })
