127790ab1f686370d510bfb8f252f827addb9386
Richard Ng <richardcrng@gmail.com>
2019-04-10 10:01:15 +0100

    write findActionRouteAndModifier tests in BDD form

--- src/redux-leaves/actions/route/findActionRouteAndModifier.test.js
+++ src/redux-leaves/actions/route/findActionRouteAndModifier.test.js
@@ -1,26 +1,71 @@
 import { findActionRouteAndModifier } from './';
 
-describe("one argument provided", () => {
-  it("returns an object with route and modifier properties", () => {
-    expect(findActionRouteAndModifier("app/with/some/ACTION")).toEqual({
-      route: ["app", "with", "some"],
-      modifier: "ACTION"
+describe("**Feature**: when provided one argument, splits it into a route array and modifier string", () => {
+
+  describe("GIVEN a string, 'app/with/some/ACTION'", () => {
+    const string = "app/with/some/ACTION"
+
+    describe("WHEN the string is passed to findActionRouteAndModifier", () => {
+      const result = findActionRouteAndModifier(string)
+
+      test("THEN it returns an object with route ['app', 'with', 'some'] and modifier 'ACTION'", () => {
+        expect(result.route).toEqual(['app', 'with', 'some'])
+        expect(result.modifier).toEqual('ACTION')
+      })
+    })
+  })
+
+  describe("GIVEN a string, 'app/ACTION'", () => {
+    const string = "app/ACTION"
+
+    describe("WHEN the string is passed to findActionRouteAndModifier", () => {
+      const result = findActionRouteAndModifier(string)
+
+      test("THEN it returns an object with route ['app'] and modifier 'ACTION'", () => {
+        expect(result.route).toEqual(['app'])
+        expect(result.modifier).toEqual('ACTION')
+      })
+    })
+  })
+
+  describe("GIVEN a string, 'ACTION'", () => {
+    const string = "ACTION"
+
+    describe("WHEN the string is passed to findActionRouteAndModifier", () => {
+      const result = findActionRouteAndModifier(string)
+
+      test("THEN it returns an object with route [] and modifier 'ACTION'", () => {
+        expect(result.route).toEqual([])
+        expect(result.modifier).toEqual('ACTION')
+      })
+    })
+  })
+
+  describe("GIVEN a string, '/ACTION/'", () => {
+    const string = "/ACTION/"
+
+    describe("WHEN the string is passed to findActionRouteAndModifier", () => {
+      const result = findActionRouteAndModifier(string)
+
+      test("THEN it returns an object with route [] and modifier 'ACTION'", () => {
+        expect(result.route).toEqual([])
+        expect(result.modifier).toEqual('ACTION')
+      })
     })
   })
 })
 
-describe("two arguments provided", () => {
-  it("returns an object with route appended by the meta path", () => {
-    expect(findActionRouteAndModifier("app/with/some/ACTION", "meta/path/and")).toEqual({
-      route: ["app", "with", "some", "meta", "path", "and"],
-      modifier: "ACTION"
-    })
-  })
+describe("**Feature**: when provided two arguments, uses the second to append to the route", () => {
+  describe("GIVEN string = 'app/with/some/ACTION", () => {
+    const string = "app/with/some/ACTION"
 
-  it("deals with excessive separators", () => {
-    expect(findActionRouteAndModifier("/app/with/some/ACTION/", "/meta/path/and/")).toEqual({
-      route: ["app", "with", "some", "meta", "path", "and"],
-      modifier: "ACTION"
+    describe("WHEN it is called with string and a second argument, 'meta/path/for'", () => {
+      const result = findActionRouteAndModifier(string, 'meta/path/for')
+
+      test("THEN it returns route ['app', 'with', 'some', 'meta', 'path', 'for'] and modifier ACTION", () => {
+        expect(result.route).toEqual(['app', 'with', 'some', 'meta', 'path', 'for'])
+        expect(result.modifier).toEqual("ACTION")
+      })
     })
   })
 })
