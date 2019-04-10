00ce89a12f0cc01cc8031d3848a6ca66b960559c
Richard Ng <richardcrng@gmail.com>
2019-04-10 18:02:14 +0100

    setup optional augmentation of combinedReducerLeaves with actions

--- src/redux-leaves/reducer/combine/combineReducerLeaves.js
+++ src/redux-leaves/reducer/combine/combineReducerLeaves.js
@@ -3,7 +3,7 @@ import { combineReducers } from "redux";
 import { withActions } from '../../actions/set/actionSet';
 
 export const combineReducerLeaves = (dict = {}, { prefix = "", route = [] } = {}, augment = true) => {
-  const reducer = combineReducers(recursivelyCombinedDict(dict, prefix, route))
+  const reducer = combineReducers(recursivelyCombinedDict(dict, { prefix, route }, augment))
   // const reducer = combineReducers(dict)
   reducer.children = _.mapValues(dict, (reducer, key) => (
     reducer.children
@@ -17,12 +17,12 @@ const augmentWithActions = (reducer, path, shouldAugment = true) => (
   shouldAugment ? withActions(reducer, path) : reducer
 )
 
-const recursivelyCombinedDict = (dict = {}, prefix, route) => {
+const recursivelyCombinedDict = (dict = {}, { prefix, route } = {}, augment) => {
   return _.mapValues(dict, (val, key) => {
     if (typeof val === "function") {
       return val
     } else if (typeof val === "object") {
-      return combineReducerLeaves(val, prefix, [...route, key])
+      return combineReducerLeaves(val, { prefix, route: [...route, key] }, augment)
     } else {
       return val
     }
