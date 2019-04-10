1d18b713cb32e73e315319ef86fdb50f4ded1208
Richard Ng <richardcrng@gmail.com>
2019-04-10 15:02:04 +0100

    create PUSH atomic action constant

--- src/redux-leaves/actions/atomic/atomicActions.js
+++ src/redux-leaves/actions/atomic/atomicActions.js
@@ -4,6 +4,7 @@ export const atomicActions = {
   INCREMENT: "INCREMENT",   // increments slice of state by action's payload
   OFF: "OFF",               // updates slice of state to false
   ON: "ON",                 // updates slice of state to true
+  PUSH: "PUSH",             // non-mutative push of payload to state
   RESET: "RESET",           // updates slice of state to initial value
   SET: "SET",               // updates slice of state to action's payload
   TOGGLE: "TOGGLE"          // updates slice of state to !current
