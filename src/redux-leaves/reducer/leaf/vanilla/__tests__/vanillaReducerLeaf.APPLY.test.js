c232532d26301d8fe90bbea232f3af2c569094de
Richard Ng <richardcrng@gmail.com>
2019-04-10 14:48:32 +0100

    write test to pass second argument of action to APPLY call

--- src/redux-leaves/reducer/leaf/vanilla/__tests__/vanillaReducerLeaf.APPLY.test.js
+++ src/redux-leaves/reducer/leaf/vanilla/__tests__/vanillaReducerLeaf.APPLY.test.js
@@ -1,6 +1,6 @@
 import { vanillaReducerLeaf } from "..";
 
-describe("**Feature**: created reducer invokes payload (function) non-mutatively with (state, action) when an action is dispatched with '.../APPLY' at the exact path implied by the prefix, route and meta", () => {
+describe("**Feature**: created reducer invokes payload (function) with (state, action) when an action is dispatched with '.../APPLY' at the exact path implied by the prefix, route and meta", () => {
 
   describe("GIVEN a reducer from { prefix: 'app/prefix', route: ['path', 'deep'], initialState: 10 }", () => {
     const reducer = vanillaReducerLeaf({ prefix: "app/prefix", route: ["path", "deep"], initialState: 10 })
@@ -12,6 +12,21 @@ describe("**Feature**: created reducer invokes payload (function) non-mutatively
         expect(result).toBe(10)
       })
     })
+
+    describe("WHEN the reducer is called with state = 5, action type 'app/prefix/path/deep/APPLY', payload = (state, { n }) => state*meta and n = 3", () => {
+      const result = reducer(5, { type: "app/prefix/path/deep/APPLY", payload: (state, { n }) => state*n, n: 3 })
+
+      test("THEN it returns 15", () => {
+        expect(result).toBe(15)
+      })
+    })
+  })
+})
+
+describe("**Feature**: created reducer invokes typically mutative functions in a non-mutative way", () => {
+
+  describe("GIVEN a reducer from { prefix: 'app/prefix', route: ['path', 'deep'], initialState: 10 }", () => {
+    const reducer = vanillaReducerLeaf({ prefix: "app/prefix", route: ["path", "deep"], initialState: 10 })
 
     describe("WHEN the reducer is called with state = ['a', 'b'], action type 'app/prefix/path/deep/APPLY' and (a typically mutative) payload = arr => arr.reverse()", () => {
       const prevState = ['a', 'b']
