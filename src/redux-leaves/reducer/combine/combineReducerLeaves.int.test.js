e4a0c657c7d5f2bd371ac53ed54a3db45bfef405
Richard Ng <richardcrng@gmail.com>
2019-04-11 08:17:52 +0100

    further integration tests for combineReducerLeaves

--- src/redux-leaves/reducer/combine/combineReducerLeaves.int.test.js
+++ src/redux-leaves/reducer/combine/combineReducerLeaves.int.test.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux';
 
 
 describe("**Feature**: it can combine a dictionary of nested reducers made via makeReducerLeaf", () => {
-  describe("GIVEN nested initialState shape with non-null values", () => {
+  describe("GIVEN nested initialState with non-null values for shape { bool, counter, foo: { value, nesting: { deep, manageable } } }", () => {
     const initialState = {
       bool: true,
       counter: 0,
@@ -44,6 +44,24 @@ describe("**Feature**: it can combine a dictionary of nested reducers made via m
             expect(reducer.children).toBeDefined()
           })
 
+          test("AND reducer.children.bool is a function", () => {
+            expect(typeof reducer.children.bool).toBe("function")
+          })
+
+          test("AND reducer.children.bool.update is an action creator of type 'bool/SET'", () => {
+            expect(typeof reducer.children.bool.update).toBe("function")
+            expect(reducer.children.bool.update.type).toBe("bool/SET")
+          })
+
+          test("AND reducer.children.foo.value.reset is an action creator of type 'foo/value/RESET'", () => {
+            expect(typeof reducer.children.foo.value.reset).toBe("function")
+            expect(reducer.children.foo.value.reset.type).toBe("foo/value/RESET")
+          })
+
+          test("AND reducer.children.foo.value.update('foobar') returns an action with payload 'foobar'", () => {
+            expect(reducer.children.foo.value.update('foobar')).toEqual({ type: "foo/value/SET", payload: "foobar" })
+          })
+
           describe("AND store = createStore(reducer)", () => {
             let store
             beforeEach(() => {
@@ -54,7 +72,7 @@ describe("**Feature**: it can combine a dictionary of nested reducers made via m
               expect(store.getState()).toEqual(initialState)
             })
 
-            describe("AND an action to clear only bool slice of state is dispatched", () => {
+            describe("AND an action to clear only bool slice of state is dispatched (reducer.children.bool.clear())", () => {
               beforeEach(() => {
                 store.dispatch(reducer.children.bool.clear())
               })
@@ -67,7 +85,21 @@ describe("**Feature**: it can combine a dictionary of nested reducers made via m
               })
             })
 
-            describe("AND an action to clear only foo.nesting slice of state is dispatched", () => {
+            describe("AND an action to update only foo.value sub-slice of state is dispatched (reducer.children.foo.value.update())", () => {
+              beforeEach(() => store.dispatch(reducer.children.foo.value.update("foobar")))
+
+              test("THEN only foo.value is updated in the store's state", () => {
+                expect(store.getState()).toEqual({
+                  ...initialState,
+                  foo: {
+                    ...initialState.foo,
+                    value: "foobar"
+                  }
+                })
+              })
+            })
+
+            describe("AND an action to clear only foo.nesting sub-slice of state is dispatched", () => {
               beforeEach(() => {
                 store.dispatch(reducer.children.foo.nesting.clear())
               })
