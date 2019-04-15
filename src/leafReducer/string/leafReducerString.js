7df72a40959999b5eed66c57efe53e1778b8c0e7
Richard Ng <richardcrng@gmail.com>
2019-04-15 15:22:04 +0100

    add replace method and test

--- src/leafReducer/string/leafReducerString.js
+++ src/leafReducer/string/leafReducerString.js
@@ -4,8 +4,11 @@ import { atomicActions } from "../../actions/atomic";
 export const leafReducerString = (leafState, { modifier, payload }) => {
   switch (modifier) {
     case atomicActions.CONCAT: return concat(leafState, ...payload)
+    case atomicActions.REPLACE: return replace(leafState, payload)
     default: return leafState
   }
 }
 
 const concat = (leafState, ...strings) => leafState.concat(...strings)
+
+const replace = (leafState, { pattern, replacement }) => leafState.replace(pattern, replacement)
