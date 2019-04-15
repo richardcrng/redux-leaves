2d913935bec5658e6d7e8329e8141bb7688d30a6
Richard Ng <richardcrng@gmail.com>
2019-04-15 12:22:50 +0100

    get leaf.create.on tests to pass

--- src/reduxLeaves/reduxLeaves.js
+++ src/reduxLeaves/reduxLeaves.js
@@ -30,6 +30,7 @@ const reduceLeaf = (leafState, { path, modifier, payload }, wholeState) => {
     case atomicActions.DROP: return drop(leafState, payload)
     case atomicActions.INCREMENT: return increment(leafState, payload)
     case atomicActions.OFF: return off(leafState)
+    case atomicActions.ON: return on(leafState)
     default: return leafState
   }
 }
@@ -54,12 +55,12 @@ const clear = (leafState, toNull) => {
   }
 }
 
-const concat = (leafState, payload) => (
-  leafState.concat(payload)
-)
+const concat = (leafState, payload) => leafState.concat(payload)
 
 const drop = (leafState, n) => _.drop(leafState, n)
 
 const increment = (leafState, n) => leafState + n
 
 const off = () => false
+
+const on = () => true
