209e6026c9e45cea20d8a1a94977c38c3c232f0b
Richard Ng <richardcrng@gmail.com>
2019-04-30 21:26:52 +0100

    add CREATOR_KEY prop to leaf and make leafReducers.spec pass

--- src/reducersDict/standardise/defaults/leafReducerDefaults.js
+++ src/reducersDict/standardise/defaults/leafReducerDefaults.js
@@ -3,9 +3,7 @@ const changeCase = require('change-case')
 const argsToPayload = first => first
 
 const type = (leaf, payload) => {
-  const { path, condition, creatorKey } = leaf;
-  const CREATOR_KEY = changeCase.constantCase(creatorKey)
-
+  const { path, condition, CREATOR_KEY } = leaf;
   const suffix = condition
     ? `${condition}.${CREATOR_KEY}`
     : CREATOR_KEY
