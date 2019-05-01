ed51d76d4b3a34dcf99c4d8676a997a7e8b1116c
Richard Ng <richardcrng@gmail.com>
2019-05-01 07:53:33 +0100

    rename to actionType

--- src/actions/template/makeActionTemplate.js
+++ src/actions/template/makeActionTemplate.js
@@ -3,12 +3,12 @@ import { leafReducerDefaults } from "../../reducersDict/standardise/defaults";
 const changeCase = require('change-case')
 
 export const makeActionTemplate = (path = [], { condition, custom } = {}) => {
-  return (creatorKey, payload, typeDef = leafReducerDefaults.type) => {
+  return (creatorKey, payload, actionType = leafReducerDefaults.actionType) => {
     const CREATOR_KEY = changeCase.snakeCase(creatorKey).toUpperCase()
     const leaf = { path, condition, creatorKey, CREATOR_KEY, custom }
-    const type = (typeof typeDef === "function")
-      ? typeDef(leaf, payload)
-      : typeDef
+    const type = (typeof actionType === "function")
+      ? actionType(leaf, payload)
+      : actionType
 
     return {
       leaf,
