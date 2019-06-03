855771fe839fccbedbdd4a46b98052a21156cf47
Richard Ng <richardcrng@gmail.com>
2019-06-03 17:35:06 +0100

    curry makeCustomActionCreator

--- src/actions/custom/makeCustomActions.js
+++ src/actions/custom/makeCustomActions.js
@@ -4,15 +4,15 @@ import { makeActionTemplate } from '../template/makeActionTemplate';
 export const makeCustomActions = (reducersDict = {}, pathToLeafOrBranch = []) => {
   const actionTemplate = makeActionTemplate(pathToLeafOrBranch, { custom: true })
   return R.mapObjIndexed(
-    (leafReducer, creatorKey) => makeCustomActionCreator(leafReducer, creatorKey, actionTemplate),
+    makeCustomActionCreator(actionTemplate),
     reducersDict
   )
 }
 
-const makeCustomActionCreator = (leafReducer, creatorKey, actionTemplate) => {
+const makeCustomActionCreator = R.curry((actionTemplate, leafReducer, creatorKey) => {
   const { argsToPayload } = leafReducer;
   return (...args) => {
     const payload = argsToPayload(...args)
     return actionTemplate(creatorKey, payload, leafReducer.type)
   }
-}
+})
