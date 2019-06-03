d181c480fec9c00cb65679cbed5d8ef8a487b23e
Richard Ng <richardcrng@gmail.com>
2019-06-03 18:00:56 +0100

    use lamda in leafReducerArray

--- src/leafReducer/array/leafReducerArray.js
+++ src/leafReducer/array/leafReducerArray.js
@@ -1,9 +1,10 @@
-import _ from 'lodash';
+import * as R from 'ramda'
+import * as RA from 'ramda-adjunct'
 import { atomicActions } from "../../actions/atomic";
 import { replaceAtIndex, insertAtIndex } from "../../actions/for/array/utils";
 
 export const leafReducerArray = (leafState, { creatorKey, payload }) => {
-  const state = _.isArray(leafState) ? leafState : _.toArray(leafState)
+  const state = RA.isArray(leafState) ? leafState : [leafState]
   switch (creatorKey) {
     case atomicActions.CONCAT: return concat(state, payload)
     case atomicActions.DROP: return drop(state, payload)
@@ -15,7 +16,7 @@ export const leafReducerArray = (leafState, { creatorKey, payload }) => {
 
 const concat = (leafState, payload) => leafState.concat(payload)
 
-const drop = (leafState, n) => _.drop(leafState, n)
+const drop = (leafState, n) => R.drop(n, leafState)
 
 const filter = (leafState, callback) => leafState.filter(callback)
 
