86fb5c44f8bbf2d7ecf51a84ef64852ca42b4ca9
Richard Ng <richardcrng@gmail.com>
2019-07-31 18:17:30 +0100

    refactor forNumber using ts

--- src/actions/for/number/forNumber.ts
+++ src/actions/for/number/forNumber.ts
@@ -2,7 +2,7 @@ import { atomicActions } from "../../atomic";
 import { conditions } from '../../condtions/conditions';
 import { makeActionTemplate } from "../../template/makeActionTemplate";
 
-export const forNumber = (pathToLeafOrBranch = []) => {
+export const forNumber = (pathToLeafOrBranch: string[] = []) => {
   const actionTemplate = makeActionTemplate(
     pathToLeafOrBranch,
     { condition: conditions.NUMBER }
