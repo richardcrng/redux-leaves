4663e2fd59828293b962b2a7eba16965e47f6566
Richard Ng <richardcrng@gmail.com>
2019-07-31 18:20:56 +0100

    refactor forString using ts

--- src/actions/for/string/forString.ts
+++ src/actions/for/string/forString.ts
@@ -2,15 +2,15 @@ import { atomicActions } from "../../atomic";
 import { conditions } from '../../condtions/conditions';
 import { makeActionTemplate } from "../../template/makeActionTemplate";
 
-export const forString = (pathToLeafOrBranch = []) => {
+export const forString = (pathToLeafOrBranch: string[] = []) => {
   const actionTemplate = makeActionTemplate(
     pathToLeafOrBranch,
     { condition: conditions.STRING }
   )
 
-  const concat = (...strings) => actionTemplate(atomicActions.CONCAT, strings)
+  const concat = (...strings: string[]) => actionTemplate(atomicActions.CONCAT, strings)
 
-  const replace = (pattern, replacement) => actionTemplate(atomicActions.REPLACE, { pattern, replacement })
+  const replace = (pattern: string | RegExp, replacement: string) => actionTemplate(atomicActions.REPLACE, { pattern, replacement })
 
   return {
     concat,
