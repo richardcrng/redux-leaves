ce38b25afc575e1019aa8e268022e515e35609cd
Richard Ng <richardcrng@gmail.com>
2019-08-01 10:00:50 +0100

    create StateBranch and Tree types

--- src/types/State/Branch/StateBranch.type.ts
+++ src/types/State/Branch/StateBranch.type.ts
@@ -1,5 +1,10 @@
-type StateBranch<T = any> = {
-  [key: string]: T
+import Dict from "../../Dict";
+import StateLeaf from "../Leaf";
+
+type StateBranch = StateBranchDict & Dict<StateLeaf>
+
+interface StateBranchDict {
+  [key: string]: StateBranch
 }
 
 export default StateBranch
