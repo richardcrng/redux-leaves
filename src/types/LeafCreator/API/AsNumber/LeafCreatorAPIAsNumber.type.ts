e159e5b2adbad43fd450482126016061b20a6ad8
Richard Ng <richardcrng@gmail.com>
2019-08-01 08:59:05 +0100

    create LeafCreatorAPIAsNumber type

---
+++ src/types/LeafCreator/API/AsNumber/LeafCreatorAPIAsNumber.type.ts
@@ -0,0 +1,7 @@
+import LeafStandardAction from "../../../LeafStandardAction";
+
+type LeafCreatorAPIAsNumber = {
+  increment(n?: number): LeafStandardAction
+}
+
+export default LeafCreatorAPIAsNumber
--- src/types/LeafCreator/API/LeafCreatorAPI.type.ts
+++ src/types/LeafCreator/API/LeafCreatorAPI.type.ts
@@ -2,10 +2,12 @@ import LeafCreatorAPIUniversal from './Universal';
 import LeafCreatorAPIAsArray from './AsArray/LeafCreatorAPIAsArray.type';
 import LeafCreatorAPIAsBoolean from './AsBoolean';
 import LeafCreatorAPIAsString from './AsString';
+import LeafCreatorAPIAsNumber from './AsNumber';
 
 type LeafCreatorAPI = LeafCreatorAPIUniversal & {
   asArray: LeafCreatorAPIAsArray
   asBoolean: LeafCreatorAPIAsBoolean
+  asNumber: LeafCreatorAPIAsNumber
   asString: LeafCreatorAPIAsString
 }
 
