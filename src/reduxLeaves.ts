d381c65e54398b4333a19659654ddf07da6ce248
richardcrng@gmail.com <richardcrng@gmail.com>
2019-09-01 15:41:11 +0100

    remove actionsFor in place of ActionsProxy

--- src/reduxLeaves.ts
+++ src/reduxLeaves.ts
@@ -1,5 +1,3 @@
-import * as R from 'ramda'
-import { actionsFor } from './actions';
 import { leafReducer } from './leafReducer';
 import standardiseReducersDict from './reducersDict/standardise';
 import { getState, updateState } from './utils';
@@ -7,6 +5,7 @@ import LeafStandardAction from './types/Actions/LSA';
 import LeafActionData from './types/Leaf/Action/Data';
 import Dict from './types/Dict';
 import { Reducer } from 'redux';
+import ActionsProxy from './actions/proxy';
 
 export const reduxLeaves = (initialState: Dict<any>, reducersDict = {}): [Reducer<any, LeafStandardAction>, Dict<any>] => {
   const leafReducersDict = standardiseReducersDict(reducersDict)
@@ -29,7 +28,7 @@ export const reduxLeaves = (initialState: Dict<any>, reducersDict = {}): [Reduce
     return updateState(state, path, newLeafState)
   }
 
-  const actions = actionsFor(R.clone(initialState), leafReducersDict)
+  const actions = new ActionsProxy(initialState, leafReducersDict)
 
   return [reducer, actions]
 }
