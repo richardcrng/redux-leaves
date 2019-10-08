aab3186aeb98da3aa20b04e5413bc75c72abbbc9
richardcrng@gmail.com <richardcrng@gmail.com>
2019-10-08 15:58:33 +0100

    pass optional type to makeActionCreator

--- src/actions/creator/makeActionCreator.ts
+++ src/actions/creator/makeActionCreator.ts
@@ -1,10 +1,10 @@
 import FluxStandardActionCreatorTypeAware from "../../types/Actions/FSA/Creator/TypeAware";
 
 // Returns a function to create actions of given type
-export const makeActionCreator = (type: string) => {
+export function makeActionCreator<T = any>(type: string) {
   // First argument passed becomes payload property of action
   //  Second argument is object that becomes spread inside action
-  const actionCreator: FluxStandardActionCreatorTypeAware = (payload?: any, meta?: any) => ({ type, payload, meta })
+  const actionCreator: FluxStandardActionCreatorTypeAware = (payload?: T, meta?: any) => ({ type, payload, meta })
 
   actionCreator.type = type
 
