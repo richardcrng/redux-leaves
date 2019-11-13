4ee564bd993adc6a56e819f32f2feafe0dd7e79f
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-13 17:32:09 +0000

    Export reduxLeaves both named and default

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { reduxLeaves } from "./reduxLeaves";
-import { getState, updateState, resetState } from './utils/index';
-import { makeActionCreator } from './actions/creators/make/makeActionCreator';
 import bundle from './bundle';
 
 export {
   bundle,
-  makeActionCreator,
-  getState,
-  resetState,
-  updateState
+  reduxLeaves
 }
 
 export default reduxLeaves
