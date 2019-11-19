6e97ada648ac20218800166cb1606de729e23821
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-19 19:33:41 +0000

    Handle nested state pointing to an object with numeric keys in updateState

--- src/utils/update-state.test.ts
+++ src/utils/update-state.test.ts
@@ -8,4 +8,12 @@ describe('updateState', () => {
     expect(initialState).toEqual({ arr: [{ nested: true }] })
     expect(result).toEqual({ arr: [{ nested: false }] })
   })
+
+  test("Handles nested state pointing to an object with numeric keys", () => {
+    const initialState = { arr: { 0: { nested: true } } }
+
+    const result = updateState(initialState, ['arr', 0, 'nested'], false)
+    expect(initialState).toEqual({ arr: { 0: { nested: true } } })
+    expect(result).toEqual({ arr: { 0: { nested: false } } })
+  })
 })
--- src/utils/update-state.ts
+++ src/utils/update-state.ts
@@ -1,17 +1,36 @@
-import * as R from 'ramda'
+// import * as R from 'ramda'
+import produce from 'immer'
+
 import { pathIsEmpty } from './path-utils';
 
-const updateIn = (path: (string | number)[], val: any, obj: object | any[]) => R.compose(
-  // @ts-ignore
-  R.set(R.__, val, obj),
-  R.apply(R.compose),
-  R.map(R.cond([[R.is(Number), R.lensIndex], [R.T, R.lensProp]]))
-  // @ts-ignore
-)(path);
+// const updateIn = (path: (string | number)[], val: any, obj: object | any[]) => R.compose(
+//   // @ts-ignore
+//   R.set(R.__, val, obj),
+//   R.apply(R.compose),
+//   R.map(R.cond([[R.is(Number), R.lensIndex], [R.T, R.lensProp]]))
+//   // @ts-ignore
+// )(path);
 
-export const updateState = (state: any, path: (string | number)[], val: any) => (
-  (pathIsEmpty(path))
-    ? val
-    // : R.assocPath(path, val, state)
-    : updateIn(path, val, state)
-)
+const setValue = (obj: any, path: (string | number)[], value: any) => {
+  // let i;
+  const pathTo = [...path]
+  const finalProp = pathTo.pop()
+  let currentObj = obj
+  
+  pathTo.forEach(prop => {
+    if (currentObj[prop] == null) {
+      currentObj[prop] = {}
+    }
+    currentObj = currentObj[prop];
+  })
+  
+  currentObj[finalProp as string | number] = value;
+}
+
+export const updateState = (state: any, path: (string | number)[], val: any): any => {
+  if (pathIsEmpty(path)) return val
+  
+  return produce(state, (draftState: any) => {
+    setValue(draftState, path, val)
+  })
+}
