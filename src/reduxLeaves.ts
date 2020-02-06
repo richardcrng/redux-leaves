249957c692433b5a9369b54c5927044908f043b0
richardcrng@gmail.com <richardcrng@gmail.com>
2020-02-06 18:29:00 +0000

    Pass reduxLeaves type to reducer

--- src/reduxLeaves.ts
+++ src/reduxLeaves.ts
@@ -2,20 +2,20 @@ import { leafReducer } from './leafReducer';
 import standardiseReducersDict from './reducersDict/standardise';
 import { getState, updateState } from './utils';
 import LeafStandardAction from './types/Actions/LSA';
-import Dict from './types/Dict';
 import { Reducer } from 'redux';
 import ActionsProxy from './actions/proxy';
 import LeafCompoundAction from './types/Actions/LCA';
 import FluxStandardAction from './types/Actions/FSA';
+import { Dictionary } from 'ramda';
 
-export const reduxLeaves = <T = Dict<any>>(initialState: T, reducersDict = {}): [Reducer<any, FluxStandardAction | LeafStandardAction | LeafCompoundAction>, ActionsProxy] => {
+export const reduxLeaves = <T = Dictionary<any>>(initialState: T, reducersDict = {}): [Reducer<T, FluxStandardAction | LeafStandardAction | LeafCompoundAction>, ActionsProxy] => {
   const leafReducersDict = standardiseReducersDict(reducersDict)
 
-  const reducer: Reducer<any, FluxStandardAction | LeafStandardAction | LeafCompoundAction> = function(state = initialState, action: FluxStandardAction | LeafStandardAction | LeafCompoundAction) {
+  const reducer: Reducer<T, FluxStandardAction | LeafStandardAction | LeafCompoundAction> = function(state = initialState, action: FluxStandardAction | LeafStandardAction | LeafCompoundAction) {
 
     if (!isLeafAction(action)) return state
 
-    if (isLCA(action)) return action.payload.reduce(
+    if (isCompoundAction(action)) return action.payload.reduce(
       reducer,
       state
     )
@@ -47,10 +47,6 @@ function isLeafAction(action: FluxStandardAction | LeafStandardAction | LeafComp
   return action.leaf
 }
 
-function isLCA(action: FluxStandardAction | LeafStandardAction | LeafCompoundAction): action is LeafCompoundAction {
+function isCompoundAction(action: FluxStandardAction | LeafStandardAction | LeafCompoundAction): action is LeafCompoundAction {
   return isLeafAction(action) && action.leaf.compound
-}
-
-function isLSA(action: FluxStandardAction | LeafStandardAction | LeafCompoundAction): action is LeafStandardAction {
-  return isLeafAction(action) && !isLCA(action)
 }
