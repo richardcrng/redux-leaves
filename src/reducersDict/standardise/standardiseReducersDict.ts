d82ac595115462a46f33c7449746345797a652cd
richardcrng@gmail.com <richardcrng@gmail.com>
2020-02-06 19:09:59 +0000

    Type standardiseReducersDict

--- src/reducersDict/standardise/standardiseReducersDict.ts
+++ src/reducersDict/standardise/standardiseReducersDict.ts
@@ -3,27 +3,28 @@ import leafReducerDefaults from './defaults';
 import LeafReducer from '../../types/Leaf/Reducer';
 import LeafReducerConfig from '../../types/Leaf/Reducer/Config';
 import LeafReducerFunction from '../../types/Leaf/Reducer/Function';
+import { Dictionary } from 'ramda';
 
-const standardiseReducersDict = (reducersDict = {}) => {
+const standardiseReducersDict = (reducersDict: Dictionary<LeafReducer> = {}): Dictionary<LeafReducerConfig> => {
   return R.mapObjIndexed(
     defineLeafReducer,
     reducersDict
   )
 }
 
-const defineLeafReducer = (definition: LeafReducer) => {
+const defineLeafReducer = (definition: LeafReducer): LeafReducerConfig => {
   return typeof definition === "function"
     ? defineLeafReducerFromFunction(definition)
     : defineLeafReducerFromConfig(definition)
 }
 
-const defineLeafReducerFromConfig = (config: LeafReducerConfig) => ({
+const defineLeafReducerFromConfig = (config: LeafReducerConfig): LeafReducerConfig => ({
   ...leafReducerDefaults,
   ...config
 })
 
-const defineLeafReducerFromFunction = (callback: LeafReducerFunction) => (
-  defineLeafReducerFromConfig({ reducer: callback })
+const defineLeafReducerFromFunction = (reducer: LeafReducerFunction): LeafReducerConfig => (
+  defineLeafReducerFromConfig({ reducer })
 )
 
 export default standardiseReducersDict
