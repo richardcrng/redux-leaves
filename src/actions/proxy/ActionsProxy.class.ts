2842960aaab798ee22a22e269244f70d7194d916
richardcrng@gmail.com <richardcrng@gmail.com>
2020-02-07 08:43:08 +0000

    Type ActionsProxy

--- src/actions/proxy/ActionsProxy.class.ts
+++ src/actions/proxy/ActionsProxy.class.ts
@@ -1,28 +1,23 @@
-import LeafReducerConfig from "../../types/Leaf/Reducer/Config";
 import actionsCreate from "../create";
 import LeafReducerDict from "../../types/Leaf/Reducer/Dict";
 import Dict from "../../types/Dict";
 
-type PropType<TObj, TProp extends keyof TObj> = TObj[TProp];
-
-function proxyActions<S extends Dict<any> = Dict<any>>() {
-
-}
-
 class ActionsProxy<S extends Dict<any> = Dict<any>, D = LeafReducerDict> {
   constructor(stateShape: S, actionsDict: D, path: (string | number)[] = []) {
     return new Proxy<S>(stateShape, {
-      get: (obj, prop: string) => {
-        switch (prop) {
-          case 'create': return actionsCreate<D>(actionsDict, path)
-          default: return new ActionsProxy({}, actionsDict, [...path, propForPath(prop)])
-        }
+      get: (obj, prop: Extract<keyof S, string> | 'create') => {
+        if (prop === 'create') return actionsCreate<D>(actionsDict, path)
+        
+        const targetValue = stateShape[prop]
+        const proxySource = typeof stateShape === 'object'
+          ? targetValue
+          : {}
+
+        return new ActionsProxy(proxySource, actionsDict, [...path, propForPath(prop)])
       }
     })
   }
 }
-
-
 
 const propForPath = (prop: string): string | number => (
   isFixedString(prop)
