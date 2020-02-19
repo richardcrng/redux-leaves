bae8dadb767e352252b3c69c79183d31d28ecadd
richardcrng@gmail.com <richardcrng@gmail.com>
2020-02-19 16:53:13 +0000

    Refactor Actions type namespace

--- src/types/actions.type.ts
+++ src/types/actions.type.ts
@@ -4,13 +4,17 @@ import { LeafStandardAction } from "./action.type";
 
 export namespace Actions {
   export type Branch<BranchShape, TreeShape, LeafShape, ReducersDict> =
-    BranchShape extends Array<infer E> ? { create: Leaf<LeafShape, TreeShape, ReducersDict> }
-    : BranchShape extends object ? { create: Leaf<LeafShape, TreeShape, ReducersDict> } & { [K in keyof BranchShape]: Branch<BranchShape[K], TreeShape, LeafShape, ReducersDict> }
-    : { create: Leaf<LeafShape, TreeShape, ReducersDict> }
+    BranchShape extends Array<infer E>
+      ? { create: Leaf<LeafShape, TreeShape, ReducersDict> }
+      : BranchShape extends object
+        ? { create: Leaf<LeafShape, TreeShape, ReducersDict> }
+          & { [K in keyof BranchShape]: Branch<BranchShape[K], TreeShape, LeafShape, ReducersDict> }
+        : { create: Leaf<LeafShape, TreeShape, ReducersDict> }
   
-  export type Leaf<LeafShape = any, TreeShape = Dict<any>, ReducersDict = any> = CreateLeafCreators<LeafShape, TreeShape, ReducersDict> & LeafCreators<LeafShape, TreeShape, ReducersDict> & {
-    apply: (callback: (leafState: LeafShape, treeState: TreeShape) => boolean) => LeafStandardAction<(leafState: LeafShape, treeState: TreeShape) => boolean>
-  }
+  export type Leaf<LeafShape = any, TreeShape = Dict<any>, ReducersDict = any> =
+    CreateLeafCreators<LeafShape, TreeShape, ReducersDict>
+      & LeafCreators<LeafShape, TreeShape, ReducersDict>
+      & { apply: (callback: (leafState: LeafShape, treeState: TreeShape) => boolean) => LeafStandardAction<(leafState: LeafShape, treeState: TreeShape) => boolean> }
 }
 
 export type CreateLeafCreators<LeafShape = any, TreeShape = Dict<any>, ReducersDict = Dict<any>, > = (actionType?: string) => LeafCreators<LeafShape, TreeShape, ReducersDict>
