5089f73605d5f94eae5fb3daa0a8d4510040157a
richardcrng@gmail.com <richardcrng@gmail.com>
2020-08-27 18:48:49 +0100

    Add updateState test

---
+++ src/utils/update-state.test.ts
@@ -0,0 +1,19 @@
+import { updateState } from "./update-state"
+
+describe('updateState', () => {
+  test("Handles nested state pointing to an array", () => {
+    const initialState = { arr: [{ nested: true }] }
+
+    const result = updateState(initialState, ['arr', 0, 'nested'], false)
+    expect(initialState).toEqual({ arr: [{ nested: true }] })
+    expect(result).toEqual({ arr: [{ nested: false }] })
+  })
+
+  test("Handles nested state pointing to an object with numeric keys", () => {
+    const initialState = { arr: { 0: { nested: true } } }
+
+    const result = updateState(initialState, ['arr', 0, 'nested'], false)
+    expect(initialState).toEqual({ arr: { 0: { nested: true } } })
+    expect(result).toEqual({ arr: { 0: { nested: false } } })
+  })
+})
