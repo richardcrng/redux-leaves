945989468ac685d1f574563c93b2e137e167ca07
richardcrng@gmail.com <richardcrng@gmail.com>
2020-08-28 18:33:44 +0100

    Write increment type tests

---
+++ docs/defaults/increment.dts-jest.ts
@@ -0,0 +1,29 @@
+import reduxLeaves from '../../src';
+
+const initialState = {
+  bool: false,
+  num: 2,
+  str: 'foo',
+  arr: [1, 2, 3],
+  obj: { num: 5, names: [['a', 'e'], ['b, c']]}
+}
+
+const [_, actions] = reduxLeaves(initialState)
+
+// @dts-jest:group Incrememnt default creator
+{
+  // @dts-jest:fail does not exist on boolean state
+  actions.bool.create.increment
+
+  // @dts-jest:pass exists on number state
+  actions.num.create.increment
+
+  // @dts-jest:pass does not need an argument
+  actions.num.create.increment()
+
+  // @dts-jest:pass can take one numerical argument
+  actions.num.create.increment(5)
+
+  // @dts-jest:fail does not take non-numerical argument
+  actions.num.create.increment('5')
+}
