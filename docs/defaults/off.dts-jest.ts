f98f56578acc305c83fba76bdce14f690f6f6174
richardcrng@gmail.com <richardcrng@gmail.com>
2020-08-28 18:40:46 +0100

    Create off type tests

---
+++ docs/defaults/off.dts-jest.ts
@@ -0,0 +1,26 @@
+import reduxLeaves from '../../src';
+
+const initialState = {
+  bool: false,
+  num: 2,
+  str: 'foo',
+  arr: [1, 2, 3],
+  obj: { num: 5, names: [['a', 'e'], ['b, c']]}
+}
+
+const [_, actions] = reduxLeaves(initialState)
+
+// @dts-jest:group Incrememnt default creator
+{
+  // @dts-jest:pass exists on boolean state
+  actions.bool.create.off
+
+  // @dts-jest:fail does not exist on number state
+  actions.num.create.off
+
+  // @dts-jest:pass does not need an argument
+  actions.bool.create.off()
+
+  // @dts-jest:fail rejects arguments
+  actions.num.create.off(true)
+}
