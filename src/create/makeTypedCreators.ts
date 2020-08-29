2e3855ad8a6d2d702c340cea5879997407efa64d
richardcrng@gmail.com <richardcrng@gmail.com>
2020-08-29 23:48:20 +0100

    Fix some compilation errors

--- src/create/makeTypedCreators.ts
+++ src/create/makeTypedCreators.ts
@@ -1,14 +1,10 @@
-import { ArrayCreators, CreateFn } from "../types";
+import { CreateFn } from "../types";
 import makeArrayCreators from "../array/makeArrayCreators";
 import makeStringCreators from "../string/makeStringCreators";
 import { isPlainObject } from "ramda-adjunct";
 import makeObjectCreators from '../object/makeObjectCreators';
 import makeBooleanCreators from "../boolean/makeBooleanCreators";
 import makeNumberCreators from "../number/makeNumberCreators";
-
-// function makeTypedCreators<L extends Array<unknown>>(leafState: L, path: (string | number)[]): CreateFn<ArrayCreators>
-
-// function makeTypedCreators<L extends any>(leafState: L, path: (string | number)[]): CreateFn<{}>
 
 function makeTypedCreators<L>(leafState: L, path: (string | number)[]): CreateFn<any> {
   // Array creators
--- src/string/stringLeafReducer.ts
+++ src/string/stringLeafReducer.ts
@@ -3,13 +3,14 @@ import { LeafStandardAction, isClearAction } from "../types"
 import universalLeafReducer from '../universal/universalLeafReducer';
 
 function stringLeafReducer<L extends string, T, A extends LeafStandardAction>(leafState: L, treeState: T, action: A, originalState: T): L {
-
-  if (isClearAction(action)) {
-    return '' as L
-  }
   
   if (isConcatActionString(action)) {
     return leafState.concat(action.payload) as L
+  }
+
+
+  if (isClearAction(action)) {
+    return '' as L
   }
 
   return universalLeafReducer(leafState, treeState, action, originalState)
